60e54c271e5de840e98d5ec6fcf9ba1af22756b9
Anurag Garg <anurag.garg@irdeto.com>
2016-06-09 20:12:51 -0400

    Minor tweaks

--- server/src/controllers/controls.js
+++ server/src/controllers/controls.js
@@ -17,26 +17,26 @@ var client = mqtt.createClient(17091, "m12.cloudmqtt.com", {
 });
 
 client.on('connect', function() { // When connected
-  console.log("mqtt connected!")
+  console.log("MQTT connected!")
   connected = true;
 });
 
 exports.render = function(req, res) {
   res.render('admin-controls.jade');
-}
+};
 
 exports.processArrow = function(req, res) {
   var urlParts = url.parse(req.url);
-  if (urlParts.path.endsWith("arrowUp")){
+
+  if (urlParts.path.endsWith("arrowUp")) {
     client.publish('move', 'F', function() {});
-  }else if(urlParts.path.endsWith("arrowDown")){
+  } else if(urlParts.path.endsWith("arrowDown")) {
     client.publish('move', 'B', function() {});
-  }else if(urlParts.path.endsWith("arrowLeft")){
+  } else if(urlParts.path.endsWith("arrowLeft")) {
     client.publish('move', 'L', function() {});
-  }else if (urlParts.path.endsWith("arrowRight")){
+  } else if (urlParts.path.endsWith("arrowRight")) {
     client.publish('move', 'R', function() {});
   }
 
-  logger.info(urlParts);
-  res.status.send(200);
+  res.render('admin-controls.jade');
 };
